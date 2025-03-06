d82c048400488b2f50ec305b7d46c42051a8d470
Pawang Rai <94522697+PawangRai@users.noreply.github.com>
2025-03-06 11:20:24 +0000

    learnt about different datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // This treats all the code as a newer version
+
+// number => 2 to the power 53
+// bigInt => for numbers greater than 2 to the power 53
+// String => ""
+// boolean => true/false
+// null => when the value is left intentionally empty
+// undefined => when the variable is initialized, no value is stored yet
+// symbol => for uniqueness
+
+console.log(typeof undefined) // Gives undefined
+console.log(typeof null)      // Gives object because of mistake in js NULL IS NOT AN OBJECT
