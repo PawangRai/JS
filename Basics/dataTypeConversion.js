beeffc62626c0130ce5e8066d137746d00d8786a
Pawang Rai <94522697+PawangRai@users.noreply.github.com>
2025-03-08 22:01:44 +0000

    Tried different combinations of operators

--- Basics/dataTypeConversion.js
+++ Basics/dataTypeConversion.js
@@ -11,3 +11,25 @@ console.log(Boolean(score))
 
 // Empty strings => false
 // Non-empty strings => true
+
+// String Concatenation
+
+let str1 = "1";
+let str2 = "2";
+
+let str3 = str1 + str2;
+// The + operator concatenates two strings 
+// console.log(str3);
+
+// console.log("1" + 2);
+
+// console.log(1 + "2");
+
+// console.log(1 + 1 + "2");
+
+// The plus operator with a number does the conversation to string depending on the position of the numbers.
+
+console.log(+true) // This converts boolean to a number which gives 1
+console.log(+"") // This converts the empty string which is false and then converts it to a number that is 0
+
+
