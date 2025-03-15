efe2effe73f4bbfc905b411b7b67f754e99775ba
Pawang Rai <94522697+PawangRai@users.noreply.github.com>
2025-03-15 14:15:04 +0000

    learnt about the for loop

---
+++ Iterations/iterations.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++){
+    for (let j = 1; j <= 10; j++){
+        console.log(`${i}*${j} = ${i*j}`)
+    }
+}
+
+// There are also keywords: break and continue
+// break = wherever break is encountered, break out of the loop
+// continue = wherever continue is encountered, move to the next iteration of the loop
